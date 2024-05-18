a00f7a77cdfd72d895fcd974555e593ca2f8ddee
Georgio <georgetrueman64@gmail.com>
2024-05-18 18:08:03 +0300

    update

--- generator/static/js/password-animation.js
+++ generator/static/js/password-animation.js
@@ -4,10 +4,34 @@ document.addEventListener("DOMContentLoaded", function() {
         x: -100
     });
 
+    gsap.set(".h1-password", {
+        opacity: 0,
+        y: -100
+    });
+
+    gsap.set(".back-menu", {
+        opacity: 0
+    });
+
     gsap.to("#password", {
         duration: 1,
         opacity: 1,
         x: 0,
         ease: "power2.inOut"
+    });
+
+    gsap.to(".h1-password", {
+        duration: 1,
+        opacity: 1,
+        y: 0,
+        ease: "power2.inOut",
+        delay: 0.5
+    });
+
+    gsap.to(".back-menu", {
+        duration: 1,
+        opacity: 1,
+        ease: "power2.inOut",
+        delay: 1
     });
 });
