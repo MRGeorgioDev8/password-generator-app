ef9d07f78decea36703ebf9f4211f9a4ff835d01
Georgio <georgetrueman64@gmail.com>
2024-05-18 19:10:02 +0300

    new

--- generator/static/js/copyscript.js
+++ generator/static/js/copyscript.js
@@ -2,12 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const copyButton = document.getElementById('copyButton');
     const passwordElement = document.getElementById('password');
 
+    tippy(copyButton, {
+        content: 'Скопировать',
+        trigger: 'mouseenter',
+        placement: 'top',
+        delay: [200, 0],
+    });
+
     copyButton.addEventListener('click', () => {
+
         const range = document.createRange();
         range.selectNode(passwordElement);
+
         window.getSelection().removeAllRanges();
         window.getSelection().addRange(range);
+
         document.execCommand('copy');
+
         window.getSelection().removeAllRanges();
+
+        tippy(copyButton, {
+            content: 'Скопировано',
+            trigger: 'manual',
+            placement: 'top',
+            duration: 600,
+        }).show();
     });
 });
--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import random
 
 def home(request):
     lst = list(range(6, 15))
-    # print(lst)  # [6, 7, 8, 9, 10, 11, 12, 13, 14]
+
     return render(request, "generator/home.html", {'lst': lst})
 
 def password(request):
@@ -18,12 +17,10 @@ def password(request):
 
     if request.GET.get('special'):
         char.extend([chr(i) for i in range(33, 48)])
-        # print(char)
 
-    # length = int(request.GET.get('length'))
     length = int(request.GET['length'])
     psw = ''
     for i in range(length):
         psw += random.choice(char)
-    # print(psw)
+
     return render(request, "generator/password.html", {'password': psw})
