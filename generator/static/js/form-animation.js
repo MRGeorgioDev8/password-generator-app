17dbcdebb8d060aa18e43aac95c5607ff5094189
Georgio <georgetrueman64@gmail.com>
2024-05-18 16:41:05 +0300

    update

--- generator/static/js/form-animation.js
+++ generator/static/js/form-animation.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('myForm');
 
-    // Устанавливаем начальное состояние формы
     gsap.set(form, { opacity: 0, y: 20 });
 
-    // Анимация появления с эффектом bounceOut
     gsap.to(form, { opacity: 1, y: 0, duration: 1, ease: "bounce.inOut", delay: 0.5 });
 });
